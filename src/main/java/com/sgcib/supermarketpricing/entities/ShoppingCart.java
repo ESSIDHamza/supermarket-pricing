80c0b7b04bfe60fc8eef48ab9cfacc7c95870a45
Hamza ESSID <eshamza2212@gmail.com>
2019-09-02 11:02:58 +0200

    Implement the business logic for the ShoppingCart class and make sure all unit tests succeed

--- src/main/java/com/sgcib/supermarketpricing/entities/ShoppingCart.java
+++ src/main/java/com/sgcib/supermarketpricing/entities/ShoppingCart.java
@@ -3,6 +3,8 @@ package com.sgcib.supermarketpricing.entities;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * This class represents a real-world shopping cart in a supermarket to which we
@@ -25,19 +27,29 @@ public class ShoppingCart {
 	 *            negative quantity means quite simply subtracting it)
 	 */
 	public void addProduct(final Product product, final int addendQuantity) {
+		Optional.ofNullable(this.productsAndTotalQuantities.get(product)).ifPresent(augendQuantity -> {
+			this.productsAndTotalQuantities.put(product, augendQuantity + addendQuantity);
+		});
+
+		if (Objects.isNull(this.productsAndTotalQuantities.get(product))) {
+			this.productsAndTotalQuantities.put(product, addendQuantity);
+		}
 	}
 
 	/**
 	 * Removes product from this shopping cart.
 	 */
 	public void removeProduct(final Product product) {
+		this.productsAndTotalQuantities.remove(product);
 	}
 
 	/**
 	 * Calculates the customer's shopping cart total price.
 	 */
 	public BigDecimal getTotalPrice() {
-		return BigDecimal.ZERO;
+		return this.productsAndTotalQuantities.keySet().stream()
+				.map(boughtProduct -> boughtProduct.getTotalPrice(productsAndTotalQuantities.get(boughtProduct)))
+				.reduce((a, b) -> a.add(b)).get();
 	}
 
 }
